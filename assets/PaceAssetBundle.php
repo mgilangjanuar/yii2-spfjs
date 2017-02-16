881dbc75ebc58e32e8ac33ec0c3eb15c7fd1df8e
Muhammad Gilang Januar <mgilangjanuar@gmail.com>
2017-02-17 06:40:28 +0700

    add pace widget for yii2

---
+++ assets/PaceAssetBundle.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * PaceAssetBundle.php
+ * @author Muhammad Gilang Januar
+ * @link http://mgilangjanuar.com
+ */
+
+namespace yii2spf\assets;
+
+/**
+ * Class PaceAssetBundle
+ * @package mgilangjanuar\yii2spf\assets
+ */
+class PaceAssetBundle extends \yii\web\AssetBundle
+{
+
+    /**
+     * @inherit
+     */
+    public $sourcePath = '@vendor/mgilangjanuar/yii2-spfjs';
+
+    public $jsOptions=['position' => \yii\web\View::POS_END];
+
+    public function registerAssetFiles($view){
+        $this->js = 'web/libs/PACE/pace.min.js';
+        parent::registerAssetFiles($view);
+    }
+
+    /**
+     * @inherit
+     */
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+}
---
+++ widgets/PaceWidget.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * SpfAssetBundle.php
+ * @author Muhammad Gilang Januar
+ * @link http://mgilangjanuar.com
+ */
+
+namespace yii2spf\widget;
+
+use Yii;
+use yii\base\Widget;
+use yii2spf\assets\PaceAssetBundle;
+use yii\web\View;
+
+/**
+ * Class PaceWidget
+ * @package mgilangjanuar\yii2spf\widgets
+ */
+class PaceWidget extends Widget
+{
+    public $color = 'red';
+
+    public $theme = 'minimal';
+
+    public $options;
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function run()
+    {
+        if(! empty($this->options)) {
+            $this->getView()->registerJs('window.paceOptions=' . json_encode($this->options), View::POS_BEGIN);
+        }
+
+        PaceAssetBundle::register($this->getView());
+
+        $asset = Yii::$app->assetManager->publish('@vendor/mgilangjanuar/yii2-spfjs', ['forceCopy'=>YII_DEBUG]);
+        
+        $this->getView()->registerCssFile($asset[1] . '/web/libs/PACE/themes/' . $this->color . '/pace-theme-' . $this->theme . '.css');
+    }
+}
